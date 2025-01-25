1b8f8a9a98dd9b1167919e70cf2da2492a0b0281
ssihala <ssihala@tutanota.com>
2025-01-25 15:16:35 -0500

    Add shelter detail routes

---
+++ app/api/shelter/route.ts
@@ -0,0 +1,54 @@
+import { NextApiResponse } from "next";
+import { NextRequest } from "next/server";
+
+interface ShelterDetails {
+  name: string;
+  formatted_address: string;
+  formatted_phone_number: string;
+  website: string;
+}
+
+export async function GET(req: NextRequest, res: NextApiResponse) {
+  const params = req.nextUrl.searchParams;
+
+  const place_id = params.get("place_id");
+
+  if (!place_id) {
+    return new Response(
+      JSON.stringify({
+        error: "No place_id provided",
+      }),
+      {
+        status: 400,
+      }
+    );
+  }
+
+  const api_key = "AIzaSyCAYOCiKQp8Fts2p4wf_riVl5uW9g-ns9w";
+  const maps_query = `https://maps.googleapis.com/maps/api/place/details/json?place_id=${place_id}&fields=name,formatted_address,formatted_phone_number,website&key=${api_key}`;
+
+  try {
+    // Make a request to the Google Maps Geocoding API
+    const response = await fetch(maps_query);
+
+    const responseBody = await response.json();
+
+    const shelter = responseBody.result as ShelterDetails;
+    // Return the shelter array
+    return new Response(JSON.stringify(shelter), {
+      status: 200,
+      headers: { "Content-Type": "application/json" },
+    });
+  } catch (error) {
+    console.error("Error querying Google Maps:", error);
+    return new Response(
+      JSON.stringify({
+        error: "Error querying Google Maps",
+      }),
+      {
+        status: 500,
+        headers: { "Content-Type": "application/json" },
+      }
+    );
+  }
+}
