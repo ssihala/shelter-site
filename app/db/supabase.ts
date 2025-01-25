58b21346499a37f06047198be723ce179d72b22d
ssihala <ssihala@tutanota.com>
2025-01-25 16:59:10 -0500

    Add database connection

---
+++ app/db/supabase.ts
@@ -0,0 +1,13 @@
+import { createClient } from "@supabase/supabase-js";
+
+// Replace with your Supabase project credentials
+const supabaseUrl = process.env.SUPABASE_URL;
+const supabaseKey = process.env.SUPABASE_KEY;
+
+if (!supabaseUrl || !supabaseKey) {
+  throw new Error("Invalid Supabase URL or Key");
+}
+// Create and export a Supabase client instance
+const supabase = createClient(supabaseUrl, supabaseKey);
+
+export default supabase;
