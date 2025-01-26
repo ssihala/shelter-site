e61c5620f9b9b47e50e90a88fe6dbba1fc4b0a93
ShahbozbekH <126989595+ShahbozbekH@users.noreply.github.com>
2025-01-25 22:41:39 -0500

    lists from db

---
+++ app/api/account/list_of_items/route.ts
@@ -0,0 +1,19 @@
+import { NextRequest } from "next/server";
+import supabase from "@/app/db/supabase";
+
+export async function POST(req: NextRequest) {
+    const param_place_id = req.nextUrl.searchParams.get("place_id");
+
+    const { data, error } = await supabase.rpc("list_of_items", {
+        place_id: param_place_id,
+      });
+    
+      if (error) {
+        return new Response(JSON.stringify({ error: error.message }), {
+          status: 500,
+        });
+      }
+      return new Response(JSON.stringify(data), {
+        status: 200,
+      });
+    }
