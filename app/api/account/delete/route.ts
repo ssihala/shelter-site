720ecf637c13a47e31c812b08a676bda13989be4
ShahbozbekH <126989595+ShahbozbekH@users.noreply.github.com>
2025-01-25 21:33:42 -0500

    Delete

--- app/api/account/delete/route.ts
+++ app/api/account/delete/route.ts
@@ -1,10 +1,21 @@
 import supabase from "@/app/db/supabase";
+import { NextRequest } from "next/server";
 
-export async function POST(req) {
-  const { data, error } = await supabase.rpc('delete_row', { update_item_name: 'test123', update_place_id: 'test123' });
-  if (!error) {
-    console.log(data);
+//Request body: param_name, param_importance, param_place_id
+export async function POST(req: NextRequest) {
+  const { param_name, param_place_id } = await req.json();
+
+  const { data, error } = await supabase.rpc("delete_item", {
+    delete_item_name: param_name,
+    delete_place_id: param_place_id
+  });
+
+  if (error) {
+    return new Response(JSON.stringify({ error: error.message }), {
+      status: 500,
+    });
   }
-
-  return new Response(JSON.stringify(data), {});
+  return new Response(JSON.stringify(data), {
+    status: 200,
+  });
 }
