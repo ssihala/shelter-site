45282823d0775ab3e91124ecf01221089d8225a5
ssihala <ssihala@tutanota.com>
2025-01-25 20:16:36 -0500

    Insert item to db fix

--- app/api/account/add_item/route.ts
+++ app/api/account/add_item/route.ts
@@ -1,10 +1,22 @@
 import supabase from "@/app/db/supabase";
+import { NextRequest } from "next/server";
 
-export async function POST(req) {
-  const { data, error } = await supabase.rpc('add_item', {item_name: 'test123', importance: '123', place_id: 'test123'});
-  if (!error) {
-    console.log(data);
+//Request body: param_name, param_importance, param_place_id
+export async function POST(req: NextRequest) {
+  const { param_name, param_importance, param_place_id } = await req.json();
+
+  const { data, error } = await supabase.rpc("insert_item", {
+    insert_name: param_name,
+    insert_importance: param_importance,
+    insert_place_id: param_place_id,
+  });
+
+  if (error) {
+    return new Response(JSON.stringify({ error: error.message }), {
+      status: 500,
+    });
   }
-
-  return new Response(JSON.stringify(data), {});
+  return new Response(JSON.stringify(data), {
+    status: 200,
+  });
 }
