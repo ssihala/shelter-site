1e56bd73d7e47d19b5749b7b30ec74d1811f0128
ShahbozbekH <126989595+ShahbozbekH@users.noreply.github.com>
2025-01-25 21:19:05 -0500

    Signup and Update

--- app/api/account/update/route.ts
+++ app/api/account/update/route.ts
@@ -1,10 +1,21 @@
 import supabase from "@/app/db/supabase";
+import { NextRequest } from "next/server";
 
-export async function POST(req) {
-  const { data, error } = await supabase.rpc('test_update', { update_item_name: 'test123', new_importance: '1234567', update_place_id: 'test123' });
-  if (!error) {
-    console.log(data);
+export async function POST(req: NextRequest) {
+  const { param_name, param_importance, param_place_id } = await req.json();
+
+  const { data, error } = await supabase.rpc("test_update", {
+    update_item_name: param_name,
+    new_importance: param_importance,
+    update_place_id: param_place_id,
+  });
+
+  if (error) {
+    return new Response(JSON.stringify({ error: error.message }), {
+      status: 500,
+    });
   }
-
-  return new Response(JSON.stringify(data), {});
+  return new Response(JSON.stringify(data), {
+    status: 200,
+  });
 }
